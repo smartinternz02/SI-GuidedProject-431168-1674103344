acaa9240524c5c3049def595a8517a09c0aa50f2
rakeshpd98 <84494678+rakeshpd98@users.noreply.github.com>
2023-02-02 14:50:18 +0530

    Add files via upload

--- Rising Waters: A Machine Learning Approach to Flood Prediction/Flask/app.py
+++ Rising Waters: A Machine Learning Approach to Flood Prediction/Flask/app.py
@@ -1,1 +1,39 @@
 
+from flask import Flask, render_template, request
+# used to run/serve our application
+# render_template is used for rendering the html pages
+#import load from joblib to load the saved model file
+from joblib import load
+
+app=Flask(__name__) # our flask app
+#load model file
+model =load('floods.save')
+sc=load('transform.save')
+
+
+@app.route('/') # rendering the html template
+def home():
+    return render_template('home.html')
+@app.route('/predict') # rendering the html template
+def index() :
+    return render_template("index.html")
+
+
+@app.route('/data_predict', methods=['POST']) # route for our prediction
+def predict():
+    temp = request.form['temp']
+    Hum = request.form['Hum']
+    db = request.form['db']
+    ap = request.form['ap']
+    aa1 = request.form['aa1']
+
+    data = [[float(temp),float(Hum),float(db),float(ap),float(aa1)]]
+    prediction = model.predict(sc.transform(data))
+    output=prediction[0]
+    if(output==0):
+        return render_template('noChance.html', prediction='No possibility of severe flood')
+    else:
+        return render_template('chance.html', prediction='possibility of severe flood')
+
+if __name__ == '__main__':
+    app.run(debug=True)
